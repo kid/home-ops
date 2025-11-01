c6f3c1cf27ea4924e22f4a04b92fdd49cb5b2ac5
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-11-01 14:38:00 +0100

    fix: pre_destroy hook not working for router

--- tf-stacks/dev/network/hook_pre_destroy.sh
+++ tf-stacks/dev/network/hook_pre_destroy.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-if tofu state list | grep -q 'routeros_ip_address'; then
+if tofu state list | grep -q 'routeros_ip_address.oob'; then
   tofu state rm routeros_ip_address.oob
 fi
 
