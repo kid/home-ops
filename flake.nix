c50d3be6c7316b890ccb447e94b9afbcc399e89c
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-12-01 16:34:14 +0100

    chore: configure formatter, remove devenv

--- flake.nix
+++ flake.nix
@@ -2,35 +2,40 @@
   description = "Description for the project";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    devshell.url = "github:numtide/devshell";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
-    devenv.url = "github:cachix/devenv";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
-        inputs.devenv.flakeModule
         inputs.treefmt-nix.flakeModule
       ];
 
       systems = [
         "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
       ];
 
       perSystem =
+        { config, pkgs, ... }:
         {
-          pkgs,
-          ...
-        }:
-        {
-          devenv.shells.default = {
+          treefmt = {
+            flakeCheck = true;
+            programs = {
+              nixfmt.enable = true;
+              hclfmt.enable = true;
+              terraform.enable = true;
+              terraform.includes = [
+                "*.tofu"
+                "*.tfvars"
+                "*.tftest.hcl"
+              ];
+            };
+          };
+          devShells.default = pkgs.mkShellNoCC {
             packages = with pkgs; [
               just
               expect
@@ -38,14 +43,15 @@
               opentofu
               tofu-ls
               terragrunt
+
+              nil
+              nixd
             ];
 
-            languages = {
-              nix.enable = true;
-            };
+            inputsFrom = [ config.treefmt.build.devShell ];
 
-            scripts.ssh.exec = ''
-              ${pkgs.openssh}/bin/ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" "$@"
+            shellHook = ''
+              alias ssh='ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no"'
             '';
           };
         };
